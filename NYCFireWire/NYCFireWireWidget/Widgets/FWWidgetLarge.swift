5129ef73c8e06a42b2111111d28f3e825bbd1b01
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-10-22 10:19:08 -0500

    All three widget sizes

---
+++ NYCFireWire/NYCFireWireWidget/Widgets/FWWidgetLarge.swift
@@ -0,0 +1,134 @@
+//
+//  FWWidgetLarge.swift
+//  NYCFireWireWidgetExtension
+//
+//  Created by Alex Rhodes on 10/22/20.
+//  Copyright © 2020 Pioneer Mobile Applications, LLC. All rights reserved.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct FWWidgetLarge: View {
+    let content: WidgetContent
+    @State var incidents = [Incident]()
+    let font = Font.custom("Avenir-Medium", size: 12)
+    let fontBold = Font.custom("Avenir-Bold", size: 12)
+    
+    var subtitleFormatted: String = ""
+    
+    func createIncidents() {
+        incidents.append(content.incident1)
+        incidents.append(content.incident2)
+        incidents.append(content.incident3)
+        
+    }
+    
+    func formatSubtitle(str: String) -> String {
+        var subtitle = ""
+        let sub = str.split(separator: " ")
+        for num in 0..<3 {
+            subtitle.append(contentsOf: sub[num])
+            subtitle.append(" ")
+        }
+        let lastSpace = subtitle.lastIndex(of: " ")!
+        subtitle.remove(at: lastSpace)
+        subtitle.append("...")
+        
+        return subtitle
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+        Color.black
+            .ignoresSafeArea() // Ignore just for the color
+            .overlay(
+                VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(Color.white)
+                            .opacity(0.15)
+                        VStack(alignment: .center, spacing: 8) {
+                            HStack {
+                                CustomText(content: content.incident1.boro, color: .red, font: font, scale: true, width: nil)
+                                CustomText(content: content.incident1.boxNumber, color: .red, font: font, scale: true, width: nil)
+                            }
+                            
+                            VStack {
+                                CustomText(content: content.incident1.title, color: .white, font: font, scale: true, width: nil)
+                                if let subtitle = content.incident1.subtitle {
+                                    CustomText(content: subtitle, color: .gray, font: font, scale: true, width: 100)
+                                }
+                                
+                            }
+                            CustomText(content: content.incident1.address, color: .red, font: font, scale: true, width: 120)
+                            
+                            
+                        }
+                    }
+                    
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(Color.white)
+                            .opacity(0.1)
+                        
+                        
+                        VStack(alignment: .center, spacing: 8) {
+                            HStack {
+                                CustomText(content: content.incident2.boro, color: .red, font: font, scale: true, width: nil)
+                                CustomText(content: content.incident2.boxNumber, color: .red, font: font, scale: true, width: nil)
+                            }
+                            
+                            VStack {
+                                CustomText(content: content.incident2.title, color: .white, font: font, scale: true, width: nil)
+                                if let subtitle = content.incident2.subtitle {
+                                    CustomText(content: subtitle, color: .gray, font: font, scale: true, width: 100)
+                                }
+                            }
+                            CustomText(content: content.incident2.address, color: .red, font: font, scale: true, width: 120)
+                        }
+                        .padding(10)
+                    }
+                }
+                    
+                    HStack(spacing: 0) {
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(Color.white)
+                                .opacity(0.05)
+                            VStack(alignment: .center, spacing: 8) {
+                                HStack {
+                                    CustomText(content: content.incident3.boro, color: .red, font: font, scale: true, width: nil)
+                                    CustomText(content: content.incident3.boxNumber, color: .red, font: font, scale: true, width: nil)
+                                }
+                                
+                                VStack {
+                                    CustomText(content: content.incident3.title, color: .white, font: font, scale: true, width: nil)
+                                    if let subtitle = content.incident3.subtitle {
+                                        CustomText(content: subtitle, color: .gray, font: font, scale: true, width: 50)
+                                    }
+                                    
+                                }
+                                CustomText(content: content.incident3.address, color: .red, font: font, scale: true, width: 120)
+                            }
+                        }
+                        Image("logo")
+
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+
+struct FWWidgetLarge_Previews: PreviewProvider {
+    static var previews: some View {
+        FWWidgetLarge(content: Incident.placeholder).previewContext(WidgetPreviewContext(family: .systemLarge))
+    }
+}
---
+++ NYCFireWire/NYCFireWireWidget/Widgets/FWWidgetMedium.swift
@@ -0,0 +1,21 @@
+//
+//  FWWidgetMedium.swift
+//  NYCFireWireWidgetExtension
+//
+//  Created by Alex Rhodes on 10/22/20.
+//  Copyright © 2020 Pioneer Mobile Applications, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct FWWidgetMedium: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FWWidgetMedium_Previews: PreviewProvider {
+    static var previews: some View {
+        FWWidgetMedium()
+    }
+}
